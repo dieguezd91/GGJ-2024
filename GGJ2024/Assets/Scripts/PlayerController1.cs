bb3094a07048677b11c56331e2d1cb6521b1ff64
Tomas Taboada <105396632+ElTabor@users.noreply.github.com>
2024-01-24 19:29:36 -0300

    Input Fix

--- GGJ2024/Assets/Scripts/PlayerController1.cs
+++ GGJ2024/Assets/Scripts/PlayerController1.cs
@@ -7,18 +7,17 @@ public class PlayerController1 : MonoBehaviour
     public float player1HorizontalInput;
     public float xLimit = 8.5f;
     public float speed = 10f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        player1HorizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate((Vector3.right * player1HorizontalInput * Time.deltaTime * speed));
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate((Vector3.right * Time.deltaTime * speed));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate((Vector3.left * Time.deltaTime * speed));
+        }
 
         if (transform.position.x < -xLimit)
         {
--- GGJ2024/Assets/Scripts/PlayerController2.cs
+++ GGJ2024/Assets/Scripts/PlayerController2.cs
@@ -7,14 +7,7 @@ public class PlayerController2 : MonoBehaviour
 {
     public float xLimit = 8.5f;
     public float speed = 10f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))
