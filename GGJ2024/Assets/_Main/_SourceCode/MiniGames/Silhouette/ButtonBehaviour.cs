05801f6690f82904acfdd82b598c71a50cac9ca8
AlenKraemer <alenkraemer@gmail.com>
2024-01-27 18:42:11 -0300

    Added variable alpha

--- GGJ2024/Assets/_Main/_SourceCode/MiniGames/Silhouette/ButtonBehaviour.cs
+++ GGJ2024/Assets/_Main/_SourceCode/MiniGames/Silhouette/ButtonBehaviour.cs
@@ -7,23 +7,36 @@ public class ButtonBehaviour : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float rotSpeed;
+    [SerializeField] private float alphaChangeSpeed;
+    private SpriteRenderer spriteRenderer;
+    private Color color = new Color(255,255,255);
     private Transform pos;
     private Rigidbody2D rb;
     private Vector3 randomDir;
     private int randomRot;
+    private float randomAlpha;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         pos = GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         GetRandomDirection();
         GetRandomRotation();
+        GetRandomAlpha();
         rb.AddForce(randomDir * speed,ForceMode2D.Impulse);
     }
 
     private void Update()
     {
         transform.Rotate(Vector3.forward, randomRot * rotSpeed * Time.deltaTime);
+        color.a += alphaChangeSpeed * randomAlpha * Time.deltaTime;
+        spriteRenderer.color = color;
+        if(color.a > 1 || color.a < 0)
+        {
+            alphaChangeSpeed *= -1f;
+        }
+       
     }
 
     public void GetRandomDirection()
@@ -36,5 +49,10 @@ public class ButtonBehaviour : MonoBehaviour
     public void GetRandomRotation()
     {
         randomRot = Random.Range(-1, 2) * 2-1;
+    }
+
+    public void GetRandomAlpha()
+    {
+        randomAlpha = Random.Range(0.1f, 1f);
     }
 }
