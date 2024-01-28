746514a46e76adf4a2b33967cc1cae42a1402970
Daniel Dieguez <dieguezd91@gmail.com>
2024-01-28 20:37:51 -0300

    SFX update

--- GGJ2024/Assets/_Main/_SourceCode/MiniGames/Benson/OrangeBall.cs
+++ GGJ2024/Assets/_Main/_SourceCode/MiniGames/Benson/OrangeBall.cs
@@ -6,6 +6,13 @@ public class OrangeBall : Ball
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Collided(col.gameObject.CompareTag("Rigby"));
+        if(col.gameObject.CompareTag("Rigby"))
+            Collided(true);
+        else 
+        {
+            if (col.gameObject.CompareTag("Mordecai"))
+                AudioManager.AudioInstance.PlaySFX("Escupir_1");
+            Collided(false);
+        }
     }
 }
